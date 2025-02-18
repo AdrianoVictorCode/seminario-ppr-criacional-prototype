6fc0e0951c09d035a29acd50692ce4cead865dca
AdrianoVictorCode <129338740+AdrianoVictorCode@users.noreply.github.com>
2025-02-17 21:42:01 -0300

    Atualizado

--- prototype.js
+++ prototype.js
@@ -1,38 +1,77 @@
-class Entity {
-    constructor(type, health, speed, damage) {
-        this.type = type;
-        this.health = health;
-        this.speed = speed;
-        this.damage = damage;
+/*
+
+*/
+
+// Classe base para itens de confeitaria
+class Pastry {
+    constructor(name) {
+        this.name = name;
     }
 
     clone() {
-        const clone = this.constructor;
+        const cloneConstructor = this.constructor;
         const properties = Object.assign({}, this);
-        return new clone(...Object.values(properties));
-    }
-
-    reset(health, speed, damage) {
-        this.health = health;
-        this.speed = speed;
-        this.damage = damage;
+        return new cloneConstructor(...Object.values(properties));
     }
 }
 
-class Enemy extends Entity {
-    constructor(health, speed, damage, ai) {
-        super("enemy", health, speed, damage);
-        this.ai = ai;
+// Produtos concretos (protótipos)
+class Cake extends Pastry {
+    constructor(name, layers) {
+        super(name);
+        this.layers = layers;
     }
 }
 
-class Player extends Entity {
-    constructor(health, speed, damage, controller) {
-        super("player", health, speed, damage);
-        this.controller = controller;
+class Cupcake extends Pastry {
+    constructor(name, flavor) {
+        super(name);
+        this.flavor = flavor;
     }
 }
 
+class Cookie extends Pastry {
+    constructor(name, size) {
+        super(name);
+        this.size = size;
+    }
+}
 
-const enemy1 = new Enemy(5, 1, 3, "melee");
-const enemy2 = enemy1.clone(); 
+// Ferramenta para gerenciar protótipos
+class PastryTool {
+    constructor() {
+        this.prototypes = {};
+    }
+
+    registerPrototype(key, prototype) {
+        this.prototypes[key] = prototype;
+    }
+
+    createPastry(key) {
+        const prototype = this.prototypes[key];
+        if (!prototype) {
+            throw new Error(`Prototype not registered for key: ${key}`);
+        }
+        return prototype.clone();
+    }
+}
+
+// Criando protótipos iniciais
+const cakePrototype = new Cake("Chocolate Cake", 3);
+const cupcakePrototype = new Cupcake("Vanilla Cupcake", "Vanilla");
+const cookiePrototype = new Cookie("Chocolate Chip Cookie", "Large");
+
+// Registrando protótipos na ferramenta
+const pastryTool = new PastryTool();
+pastryTool.registerPrototype("cake", cakePrototype);
+pastryTool.registerPrototype("cupcake", cupcakePrototype);
+pastryTool.registerPrototype("cookie", cookiePrototype);
+
+// Criando novas instâncias com base nos protótipos
+const newCake = pastryTool.createPastry("cake");
+const newCupcake = pastryTool.createPastry("cupcake");
+const newCookie = pastryTool.createPastry("cookie");
+
+console.log(newCake);
+console.log(newCupcake);
+console.log(newCookie);
